b1ddc206d6021c330cc55e9678fde79ce5541831
waqarahmed034920@gmail.com <waqarahmed034920@gmail.com>
2020-07-15 18:09:06 +0500

    adding new practice files.

---
+++ 15 July/if-functions.js
@@ -0,0 +1,28 @@
+var btn = document.getElementById('btnApply');
+var d = document.getElementById('mydiv');
+var txt = document.getElementById('txtChoice');
+btn.addEventListener('click', onApplyClick);
+
+function onApplyClick() {
+
+    var essey = "<h1>Write an essay</h1><h2>My School</h2><p>xyz is my best friend we go to school together.</p>";
+
+    if (txt.value === "1") {
+        d.innerHTML = myBestFriend();
+    } else if (txt.value === "2") {
+        d.innerHTML = mySchool();
+    } else {
+        d.innerHTML = "invalid choice";
+    }
+
+}
+
+function myBestFriend() {
+    var str = "<h1>Write an essay</h1><h2>My Best Friend</h2><p>xyz is my best friend we go to school together.</p>";
+    return str;
+}
+
+function mySchool() {
+    var str = "<h1>Write a Poem</h1><h2>My School</h2><p>xyz is my best friend we go to school together.</p>";
+    return str;
+}
